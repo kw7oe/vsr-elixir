71177b8d4bce79fb1b95f33d81efb19281f5c0d3
kw7oe <choongkwern@hotmail.com>
2022-06-13 16:24:11 +0800

    Initialize Repository

---
+++ lib/vsr.ex
@@ -0,0 +1,18 @@
+defmodule Vsr do
+  @moduledoc """
+  Documentation for `Vsr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vsr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vsr_test.exs
@@ -0,0 +1,8 @@
+defmodule VsrTest do
+  use ExUnit.Case
+  doctest Vsr
+
+  test "greets the world" do
+    assert Vsr.hello() == :world
+  end
+end
