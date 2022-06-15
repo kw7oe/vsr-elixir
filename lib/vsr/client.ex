2974f94d937a9914538471d3c83092174e279112
kw7oe <choongkwern@hotmail.com>
2022-06-15 19:30:16 +0800

    implement client to send normal ops message

--- lib/vsr/client.ex
+++ lib/vsr/client.ex
@@ -19,8 +19,26 @@ defmodule Vsr.Client do
     {:ok, state}
   end
 
+  # Client to Server only
   def request(client, op) do
     GenServer.call(client, {:request, op})
+  end
+
+  # Server to Replica only
+
+  # Send by primary to replica upon receiving a request.
+  def prepare(client, view_number, message, op_number, commit_number) do
+    GenServer.call(client, {:prepare, view_number, message, op_number, commit_number})
+  end
+
+  # Send by replica i to primary, to reply to the prepare message from primary.
+  def prepare_ok(client, view_number, op_number, i) do
+    GenServer.call(client, {:prepare_ok, view_number, op_number, i})
+  end
+
+  # Send by primary to replica.
+  def commit(client, view_number, commit_number) do
+    GenServer.call(client, {:commit, view_number, commit_number})
   end
 
   def handle_call({:request, op}, _from, state) do
@@ -30,6 +48,30 @@ defmodule Vsr.Client do
 
     resp = tcp_send(state.conn, "request,#{op},#{c},#{s}")
     {:reply, resp, %{state | request_number: s + 1}}
+  end
+
+  def handle_call({:prepare, v, m, o, c}, _from, state) do
+    id = state.client_id
+    Logger.info("client #{id}: prepare v=#{v} m=\"#{m}\" o=#{o} c=#{c}")
+
+    resp = tcp_send(state.conn, "prepare,#{v},#{m},#{o},#{c}")
+    {:reply, resp, state}
+  end
+
+  def handle_call({:prepare_ok, v, n, i}, _from, state) do
+    id = state.client_id
+    Logger.info("client #{id}: prepare_ok v=#{v} n=\"#{n}\" i=#{i}")
+
+    resp = tcp_send(state.conn, "prepare_ok,#{v},#{n},#{i}")
+    {:reply, resp, state}
+  end
+
+  def handle_call({:commit, v, k}, _from, state) do
+    id = state.client_id
+    Logger.info("client #{id}: commit v=#{v} k=#{k}")
+
+    resp = tcp_send(state.conn, "commit,#{v},#{k}")
+    {:reply, resp, state}
   end
 
   def tcp_send(conn, data) do
