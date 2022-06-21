306aa862e388faa10f4c106fedf2012305392927
kw7oe <choongkwern@hotmail.com>
2022-06-21 10:31:12 +0800

    minor clean up and add comment

--- lib/vsr/server.ex
+++ lib/vsr/server.ex
@@ -44,6 +44,12 @@ defmodule Vsr.Server do
     Logger.info(%{state: state, client_info: client_info})
 
     # Step 2
+    #
+    # When the primary receives the request, it compares the request-number in the request
+    # with the information in the client table. If the request-number s isn’t bigger than
+    # the information in the table it drops the request, but it will re-send the response if
+    # the request is the most recent one from this client and it has already been executed.
+
     state =
       if request_number <= client_info.last_request_number do
         {last_request_number, result} = client_info.last_result
@@ -57,9 +63,22 @@ defmodule Vsr.Server do
         state
       else
         # Step 3
+        #
+        # The primary advances op-number, adds the request to the end of the log, and updates
+        # the information for this client in the client-table to contain the new
+        # request number, s. Then it sends a ⟨PREPARE v, m, n, k⟩ message to the other replicas,
+        # where v is the current view-number, m is the message it received from the client,
+        # n is the op-number it assigned to the request, and k is the commit-number.
+
         op_number = state.op_number + 1
         message = Vsr.Message.to_message(request)
         log = [to_log(op_number, message) | state.log]
+
+        # Skip update client-table... since if we failed, the state won't be
+        # stored as well.
+        #
+        # To fix this, we need to store our state on a long running process.
+        # Our current implementation pass the state around...
 
         # Send <Prepare v, m, n, k> to other replicas
         #
@@ -82,23 +101,34 @@ defmodule Vsr.Server do
           Logger.info("received reply from replica #{port}: #{reply}")
         end
 
-        # Compute result
-        result = "result#{request_number}"
+        # Step 5:
+        #
+        # The primary waits for f PREPAREOK messages from different backups;
+        # at this point it considers the operation (and all earlier ones) to be committed.
+        # Then, after it has executed all earlier operations (those assigned smaller op-numbers),
+        # the primary executes the operation by making an up-call to the service code, and
+        # increments its commit-number. Then it sends a ⟨REPLY v, s, x⟩ message to the client;
+        # here v is the view-number, s is the number the client provided in the request, and
+        # x is the result of the up-call. The primary also updates the client’s entry in the
+        # client-table to contain the result.
 
-        # Commit number is increment after primary excutes the operation
-        # by calling the service code.
+        # TODO: wait for f PrepareOk message.  Current implmentation wait for all request.
+
+        # Performing upcall to service code and increment commit number
+        result = upcall(request_number)
         commit_number = state.commit_number + 1
 
         # Send <Reply v, s, x> to client
         write_line(socket, Vsr.Message.reply(state.view_number, request_number, result))
 
+        # Update client table:
         client_table =
           Map.put(state.client_table, client_id, %Vsr.VRState.ClientInfo{
             last_request_number: request_number,
             last_result: {request_number, result}
           })
 
-        # Update
+        # Update state
         %{
           state
           | client_table: client_table,
@@ -122,6 +152,17 @@ defmodule Vsr.Server do
     # Commit
     inspect_logs(state.log)
     {commit_number, client_table} = commit(state, commit_number)
+
+    # Step 4:
+    #
+    # Backups process PREPARE messages in order: a backup won’t accept a prepare with op-number n
+    # until it has entries for all earlier requests in its log. When a backup i receives a
+    # PREPARE message, it waits until it has entries in its log for all earlier re- quests
+    # (doing state transfer if necessary to get the missing information).
+    #
+    # Then it increments its op-number, adds the request to the end of its log, updates the
+    # client’s information in the client-table, and sends a ⟨PREPAREOK v, n, i⟩ message to the
+    # primary to indicate that this operation and all earlier ones have prepared locally.
 
     # Not sure if checking op number with commmit number is enough to ensure
     # the following property:
@@ -171,7 +212,16 @@ defmodule Vsr.Server do
     write_line(socket, "unsupported message type")
   end
 
+  # Step 7:
+  #
+  # When a backup learns of a commit, it waits until it has the request in its log
+  # (which may require state transfer) and until it has executed all earlier operations.
+  # Then it executes the operation by performing the up-call to the service code,
+  # increments its commit-number, updates the client’s entry in the client-table,
+  # but does not send the reply to the client.
   defp commit(state, commit_number) do
+    # Receive instruction to commit commit_number, check if we have commited up
+    # to commit_number - 1:
     if commit_number != 0 && state.commit_number != commit_number - 1 do
       Logger.warn("commit: does not contain all the earlier operations...")
     end
@@ -192,7 +242,8 @@ defmodule Vsr.Server do
       Logger.info("committing #{commit_number}: #{message_to_commit}...")
       {_, _, client_id, request_number} = Vsr.Message.parse(message_to_commit)
 
-      result = "result#{request_number}"
+      # Performing up-call to the service code
+      result = upcall(request_number)
       commit_number = state.commit_number + 1
 
       client_table =
@@ -224,6 +275,10 @@ defmodule Vsr.Server do
 
   defp to_log(op_number, message) do
     "#{op_number}:#{message}"
+  end
+
+  defp upcall(request_number) do
+    "result#{request_number}"
   end
 
   defp inspect_logs(logs) do
